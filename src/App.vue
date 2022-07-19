fca1f3a86737c07047bef0f5ac97495fddc12e46
morita <morita@morita.local>
2022-07-19 15:51:51 +0900

    componentsの名前変更

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GraphVue msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GraphVue from './components/GraphVue.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GraphVue
   }
 }
 </script>
