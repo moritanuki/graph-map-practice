d653635f8acfae841dc5b99dcc6a5ce3c1761a7d
moritanuki <ilovfam12@gmail.com>
2022-07-20 14:42:09 +0900

    地図の表示に成功

--- src/components/MapVue.vue
+++ src/components/MapVue.vue
@@ -1,27 +1,32 @@
 <template>
  <div class="mapVue">
     <h3>Map</h3>
-    <button @click="dataPlot">Plot Line</button>
+    <button @click="mapCreate">Plot Line</button>
     <div id="map"></div>
  </div>
 </template>
 
 <script>
     import Map from 'ol/Map'
-    // import View from 'ol/View'
-    // import TileLayer from 'ol/layer/Tile'
-    // import XYZ from 'ol/source/XYZ'
-    // import { fromLonLat } from 'ol/proj'
+    import OSM from 'ol/source/OSM'
+    import View from 'ol/View'
+    import TileLayer from 'ol/layer/Tile'
 
     export default {
         name: 'mapVue',
-        mounted() {
-            this.mapCreate()
-        },
         methods: {
             mapCreate() {
                 new Map({
                     target: 'map',
+                    layers: [
+                        new TileLayer({
+                        source: new OSM(),
+                        }),
+                    ],
+                    view: new View ({
+                        center: [0, 0],
+                        zoom: 2,
+                    })
                 })
             }
         }
@@ -29,7 +34,7 @@
 </script>
 
 <style>
-    .map {
+    #map {
       height: 400px;
       width: 100%;
     }
