018cb61932aeffcb9e620f48922d200a1cdc06b3
moritanuki <ilovfam12@gmail.com>
2022-07-21 10:49:33 +0900

    jSONデータから点を描画・投影法も変更

--- src/components/MapVue.vue
+++ src/components/MapVue.vue
@@ -12,12 +12,13 @@
     import View from 'ol/View'
     import TileLayer from 'ol/layer/Tile'
     import Feature from 'ol/Feature'
+    // import {Point} from 'ol/geom'
     import {LineString, Point} from 'ol/geom'
     import {OSM, Vector as VectorSource} from 'ol/source'
     import {Vector as VectorLayer} from 'ol/layer'
 
     // 描画情報
-    // import shipData1 from '../assets/json/ship-data1.json'
+    import shipData1 from '../assets/json/ship-data1.json'
 
     export default {
         name: 'mapVue',
@@ -32,6 +33,7 @@
         methods: {
             mapCreate() {
                 // 地図を描画
+
                 this.map = new Map({
                     target: 'map',
                     layers: [
@@ -40,27 +42,86 @@
                         }),
                     ],
                     view: new View ({
-                        center: [0, 0],
+                        projection: 'EPSG:4326',
+                        center: [139.0915, 35.1878],
                         zoom: 2,
                     })
                 })
             },
-            plotLine() {
+            createPoint() {
                 // ポイントを生成
-                const pointFeature = new Feature(new Point([0, 0]))
+                let points = []
+
+                shipData1.SpasDailyData.forEach((data) => {
+                    let LONDEGMIN = data.LONDEGMIN //経度
+                    const LONEW = data.LONEW
+                    let LATDEGMIN = data.LATDEGMIN //緯度
+                    const LATNS = data.LATNS
+
+                    // null値除外
+                    if(LATDEGMIN == null || LONDEGMIN == null) {
+                        return
+                    }
+
+                    // 方角による表記変換
+                    if(LONEW === 'W') {
+                        LONDEGMIN = -(LONDEGMIN)
+                    }
+                    if(LATNS === 'S') {
+                        LATDEGMIN = -(LATDEGMIN)
+                    }
+
+                    points.push([LONDEGMIN, LATDEGMIN] )
+                })
+
+                return points
+            },
+            generateFeatures(shapes, featureType) {
+                let features = []
+
+                switch(featureType) {
+                    case 'Point':
+                        shapes.forEach((data) => {
+                            features.push(
+                                new Feature(
+                                    new Point(
+                                    data
+                                    )
+                                )
+                            )
+                        })
+                        break
+                    case 'LineString':
+                        shapes.forEach((data) => {
+                            features.push(
+                                new Feature(
+                                    new LineString(
+                                    data
+                                    )
+                                )
+                            )
+                        })
+                        break
+                }
+
+                return features
+            },
+            plotLine() {
+                
+                const pointFeatures = this.generateFeatures(this.createPoint(), 'Point')
 
                 // ラインを生成
-                const lineFeature = new Feature(
-                    new LineString([
-                        [0, 0],
-                        [0, 3e6]
-                    ])
-                )
+                // const lineFeature = new Feature(
+                //     new LineString([
+                //         [1e4, -2e2],
+                //         [22222, 3e6]
+                //     ])
+                // )
 
                 // 図形生成
                 const vector = new VectorLayer({
                     source: new VectorSource({
-                        features: [pointFeature, lineFeature]
+                        features: pointFeatures
                     })
                 })
 
