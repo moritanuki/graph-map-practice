d6ca0c9e685d3d18a54da1d6aeccad94a63864a6
moritanuki <ilovfam12@gmail.com>
2022-07-21 13:28:37 +0900

    始点・終点をアイコンに変更

--- src/components/MapVue.vue
+++ src/components/MapVue.vue
@@ -12,13 +12,15 @@
     import View from 'ol/View'
     import TileLayer from 'ol/layer/Tile'
     import Feature from 'ol/Feature'
-    // import {Point} from 'ol/geom'
     import {LineString, Point} from 'ol/geom'
     import {OSM, Vector as VectorSource} from 'ol/source'
     import {Vector as VectorLayer} from 'ol/layer'
+    import {Icon, Style} from 'ol/style'
 
     // 描画情報
     import shipData1 from '../assets/json/ship-data1.json'
+    import ship from '../assets/img/ship.png'
+    import shipBlue from '../assets/img/ship_blue.png'
 
     export default {
         name: 'mapVue',
@@ -48,7 +50,7 @@
                     })
                 })
             },
-            createPoint() {
+            createPoints() {
                 // ポイントを生成
                 let points = []
 
@@ -79,6 +81,8 @@
             },
             generateFeatures(points, featureType) {
                 // 図形クラス生成
+                // featureType: 'Point', 'LineString'
+
                 let features = []
 
                 switch(featureType) {
@@ -86,9 +90,7 @@
                         points.forEach((data) => {
                             features.push(
                                 new Feature(
-                                    new Point(
-                                    data
-                                    )
+                                    new Point(data)
                                 )
                             )
                         })
@@ -115,20 +117,48 @@
             },
             plotLine() {
 
-                const points = this.createPoint()
+                const points = this.createPoints()
                 
                 const pointFeatures = this.generateFeatures(points, 'Point')
                 const lineFeatures = this.generateFeatures(points, 'LineString')
-
+                const shipFeature = this.generateFeatures([points[points.length - 1]], 'Point')
+                const shipBlueFeature = this.generateFeatures([points[0]], 'Point')
+                
                 // 図形生成
                 const vector = new VectorLayer({
                     source: new VectorSource({
                         features: [...pointFeatures, ...lineFeatures]
+                    }),
+                })
+
+                // 始点を船アイコンで描画
+                const vectorShip = new VectorLayer({
+                    source: new VectorSource({
+                        features: shipFeature
+                    }),
+                    style: new Style({
+                        image: new Icon({
+                            src: ship
+                        })
+                    })
+                })
+
+                // 始点を青い船アイコンで描画
+                const vectorShipBlue = new VectorLayer({
+                    source: new VectorSource({
+                        features: shipBlueFeature
+                    }),
+                    style: new Style({
+                        image: new Icon({
+                            src: shipBlue
+                        })
                     })
                 })
 
                 // 地図に描画
                 this.map.addLayer(vector)
+                this.map.addLayer(vectorShip)
+                this.map.addLayer(vectorShipBlue)
             }
         }
     }
